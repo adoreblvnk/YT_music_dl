5c86b78f2e9abf98c501bbb5d6200ef9209c1a94
adoreblvnk <83258252+adoreblvnk@users.noreply.github.com>
2024-10-11 17:25:47 +0800

    fix: incorrect song titles due to wrong var

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import yt_dlp
 from mutagen.id3 import TALB, TIT2, TPE1
 from mutagen.mp3 import MP3
 
-FOLDER: str = "test"
+FOLDER: str = "music"
 URLS: list[str] = None
 
 YDL_OPTS = {
@@ -62,9 +62,9 @@ for file in Path(FOLDER).iterdir():
     # standardising title string
     title = new_file.split(" - ")[1]
     # remove | <channel>, eg Artist - Song | Pressplay
-    new_file = re.sub(r"\|.*", "", new_file)
+    title = re.sub(r"\|.*", "", title)
     # remove (.*?), eg Artist - Song (Official Music Video)
-    new_file = re.sub(r"\((official|music).*?\)", "", new_file, flags=re.IGNORECASE)
+    title = re.sub(r"\((official|music).*?\)", "", title, flags=re.IGNORECASE)
     # remove #.*?, eg Artist - Song #Album
     title = re.sub(r"#\w*", "", title)
     # remove [.*], eg Artist - Song [S2.E1]
@@ -77,7 +77,7 @@ for file in Path(FOLDER).iterdir():
     artist_list = new_file.split(" - ")[0].split(", ")
     # remove duplicate artists that appear in title
     artist_list = [i for i in artist_list if i not in title]
-    
+
     # creating artist string
     artist_str = " x ".join(artist_list)
     # use x (not X) to separate artists, eg Artist X Artist - Song
